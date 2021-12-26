d29659fc71f78a084130ae0f1c2d6348492ef26c
yurikorh <yurikorh@outlook.com>
2021-12-27 01:52:08 +0800

    Add: get direction from step

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 if [ ! -d "build" ]; then
   mkdir build
 fi
-emcc web.cc snake.cc -s USE_SDL=2 -s ASSERTIONS=1 -s SAFE_HEAP=1 -s TOTAL_MEMORY=134217728 -O3 -o build/index.html
+emcc web.cc snake.cc -s USE_SDL=2 -s ASSERTIONS=1 -s SAFE_HEAP=1 -s TOTAL_MEMORY=134217728 -s LLD_REPORT_UNDEFINED -O3 -o build/index.html
