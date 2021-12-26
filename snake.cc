0ac9c2e84bc22a7b9ce5425a82468c90eae3f4fb
yurikorh <yurikorh@outlook.com>
2021-12-26 20:28:28 +0800

    init project

---
+++ snake.cc
@@ -0,0 +1,183 @@
+#include "snake.hh"
+#include <cstring>
+#include <random>
+
+Snake::Snake(int cols, int rows)
+{
+    new (this) Snake(cols, rows, 3);
+}
+Snake::Snake(int cols, int rows, int length = 3)
+{
+    if (cols <= length)
+        cols = length + 1;
+    this->cols = cols;
+    this->rows = rows;
+    this->mapWidth = 2 << (getBits(cols + 1) - 1);
+
+    int mapSize = mapWidth * (rows + 2);
+
+    // this->map = new int8_t[mapSize];
+    this->map = (int8_t *)malloc(sizeof(int8_t) * mapSize);
+
+    memset(map, Wall, sizeof(int8_t) * mapSize);
+    this->map += this->mapWidth;
+    for (int i = 0; i < rows; i++)
+    {
+        memset(this->map + i * this->mapWidth, Ground, sizeof(int8_t) * cols);
+    }
+
+    std::random_device random;
+    int y = random() % this->rows;
+
+    this->tail = this->map + y * this->mapWidth;
+
+    this->head = this->tail + (length - 1);
+
+    this->steps[GS_RIGHT] = 1;
+    this->steps[GS_LEFT] = -1;
+    this->steps[GS_DOWN] = this->mapWidth;
+    this->steps[GS_UP] = -this->mapWidth;
+
+    for (int8_t *p = this->tail; p < this->head; ++p)
+        *p = this->getStep(GS_RIGHT);
+
+    this->step = this->getStep(GS_RIGHT);
+
+    initRandomApples(length + 1);
+    this->applePos = this->getApple();
+}
+
+Snake::~Snake()
+{
+    free(map - this->mapWidth);
+    free(apples);
+}
+int Snake::getBits(int i)
+{
+    int bits = 0;
+    while (i)
+    {
+        i >>= 1;
+        bits++;
+    }
+    return bits;
+}
+
+std::vector<RenderIns> Snake::tick(GS_DIRECTION input)
+{
+    if (input != GS_NONE)
+    {
+        GS_STEP newStep = steps[input];
+        if (newStep != this->step && newStep != -this->step)
+        {
+            this->step = newStep;
+        }
+    }
+
+    int8_t *headDst = this->head + this->step;
+
+    std::vector<RenderIns> res;
+    if (*headDst == Apple)
+    {
+        res.push_back(this->moveHead(headDst));
+
+        res.push_back(RenderIns(this->getCoordinate(this->tail), this->getCoordinate(this->tail)));
+
+        this->applePos = this->getApple();
+        res.push_back(RenderIns(this->applePos, this->applePos));
+        return res;
+    }
+    if (*headDst == Ground)
+    {
+        // move forward
+        res.push_back(this->moveHead(headDst));
+        res.push_back(this->dropTail());
+        return res;
+    }
+    // not apple or ground, game over
+    return res;
+}
+
+RenderIns Snake::moveHead(int8_t *dst)
+{
+    // std::cout << "Input Mat of tick(): " << std::endl;
+    // std::cout << *(this) << std::endl;
+    RenderIns headIns = RenderIns(
+        this->getCoordinate(this->head),
+        this->getCoordinate(dst));
+    *(this->head) = this->step;
+
+    this->head = dst;
+    *dst = Head;
+    return headIns;
+}
+
+RenderIns Snake::dropTail()
+{
+    int8_t *newTail = this->tail + *(this->tail);
+    RenderIns tailIns = RenderIns(
+        this->getCoordinate(this->tail),
+        this->getCoordinate(newTail));
+
+    *(this->tail) = Ground;
+
+    this->tail = newTail;
+    return tailIns;
+}
+
+void Snake::initRandomApples(int length)
+{
+    int total = this->size() - length;
+    this->apples = new int[total];
+    this->nextApple = this->apples;
+    std::random_device random;
+    for (int i = 0; total; i++, --total)
+        this->apples[i] = random() % total;
+}
+/**
+ * @brief get next apple's coordinates from the pre-generated random apple map
+ *
+ * @return Vec2c coordinates of the next apple
+ */
+Vec2c Snake::getApple()
+{
+    int appleIndex = *(this->nextApple);
+    this->nextApple++;
+    for (int y = 0; y < this->rows; ++y)
+    {
+        for (int x = 0; x < this->cols; ++x)
+        {
+            int offset = y * this->mapWidth + x;
+            if (this->map[offset] == Ground)
+            {
+                if (appleIndex-- == 0)
+                {
+                    this->map[offset] = Apple;
+                    return Vec2c(x, y);
+                }
+            }
+        }
+    }
+    // should never reach here, if everything works fine
+    std::cout << *(this->nextApple - 1) << std::endl;
+    return Vec2c(-1, -1);
+}
+int Snake::size()
+{
+    return this->cols * this->rows;
+}
+
+Vec2c Snake::getCoordinate(int offset)
+{
+    return Vec2c(offset % mapWidth, offset / mapWidth);
+}
+
+Vec2c Snake::getCoordinate(int8_t *pointer)
+{
+    return this->getCoordinate(pointer - this->map);
+}
+
+inline GS_STEP Snake::getStep(GS_DIRECTION dir)
+{
+    return this->steps[dir];
+}
---
+++ web.cc
@@ -0,0 +1,258 @@
+#include "web.hh"
+
+GS_DIRECTION input()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        if(event.type == SDL_KEYDOWN)
+        {
+            int32_t code = event.key.keysym.sym - SDLK_RIGHT;
+            if(code >= 0 && code < 4)
+            {
+                // 0: right 1: left 2: down 3: up
+                return GS_DIRECTION(code);
+            }
+        }
+    }
+
+    return GS_NONE;
+
+}
+
+void drawApple(context *ctx, Vec2c pos, Scalar s)
+{
+    SDL_Rect rect;
+
+    SDL_SetRenderDrawColor(ctx->renderer, s.r, s.g, s.b, 255);
+    rect.x = (pos.x) * 50;
+    rect.y = (pos.y) * 50;
+    rect.w = 50;
+    rect.h = 50;
+    SDL_RenderFillRect(ctx->renderer, &rect);
+}
+
+// static int8_t lastdir;
+void drawBody(context *ctx, int8_t *ptr, Scalar s)
+{
+    SDL_SetRenderDrawColor(ctx->renderer, s.r, s.g, s.b, 255);
+    Vec2c pos = ctx->snake->getCoordinate(ptr);
+    SDL_Rect rect = {pos.x * 50 + 2, pos.y * 50 + 2, 50 - 4, 50 - 4};
+    while (true) {
+        if(*ptr == ctx->snake->getStep(GS_RIGHT))
+        {
+            rect.w += 4;
+            break;
+        }
+        if(*ptr == ctx->snake->getStep(GS_LEFT))
+        {
+            rect.x -= 4;
+            rect.w += 4;
+            break;
+        }
+        if(*ptr == ctx->snake->getStep(GS_DOWN))
+        {
+            rect.h += 4;
+            break;
+        }
+        if(*ptr == ctx->snake->getStep(GS_UP))
+        {
+            rect.y -= 4;
+            rect.h += 4;
+            break;
+        }
+    }
+    // if (dir == lastdir)
+    // {
+    //     if (dir == 1 || dir == -1)
+    //     {
+    //         rect.y += 2;
+    //         rect.h -= 4;
+    //     }
+    //     else
+    //     {
+    //         rect.x += 2;
+    //         rect.w -= 4;
+    //     }
+    // }
+    // else
+    // {
+    //     if (lastdir == -1)
+    //     {
+    //         rect.x += 2;
+    //         rect.w -= 2;
+    //     }
+    //     else if (lastdir == 1)
+    //     {
+    //         rect.w -= 2;
+    //     }
+    //     else if (lastdir < -1)
+    //     {
+    //         rect.y += 2;
+    //         rect.h -= 2;
+    //     }
+    //     else
+    //     {
+    //         rect.h -= 2;
+    //     }
+
+    //     if (dir == -1)
+    //     {
+    //         rect.w -= 2;
+    //     }
+    //     else if (dir == 1)
+    //     {
+    //         rect.x += 2;
+    //         rect.w -= 2;
+    //     }
+    //     else if (dir < -1)
+    //     {
+    //         rect.h -= 2;
+    //     }
+    //     else
+    //     {
+    //         rect.y += 2;
+    //         rect.h -= 2;
+    //     }
+    // }
+    // lastdir = dir;
+    SDL_RenderFillRect(ctx->renderer, &rect);
+}
+
+void drawHead(context *ctx, Scalar s0, Scalar s1)
+{
+    RenderIns renderIns = ctx->ris[0];
+    int current = ctx->now - ctx->start;
+    int duration = ctx->duration;
+    int x = renderIns.start.x * (duration - current);
+    x += renderIns.end.x * current;
+    x = x * 50 / duration;
+    int y = renderIns.start.y * (duration - current);
+    y += renderIns.end.y * current;
+    y = y * 50 / duration;
+
+    SDL_Rect rect = {x, y, 50, 50};
+    SDL_SetRenderDrawColor(ctx->renderer, s0.r, s0.g, s0.b, 255);
+    SDL_RenderFillRect(ctx->renderer, &rect);
+
+    int oldX = renderIns.start.x * 50;
+    int oldY = renderIns.start.y * 50;
+    rect = {oldX, oldY, x - oldX, y - oldY};
+    SDL_SetRenderDrawColor(ctx->renderer, s1.r, s1.g, s1.b, 255);
+    SDL_RenderFillRect(ctx->renderer, &rect);
+}
+
+void drawTail(context *ctx, Scalar s)
+{
+    RenderIns renderIns = ctx->ris[1];
+    int current = ctx->now - ctx->start;
+    int duration = ctx->duration;
+    int x = renderIns.start.x * (duration - current);
+    x += renderIns.end.x * current;
+    x = x * 50 / duration;
+    int y = renderIns.start.y * (duration - current);
+    y += renderIns.end.y * current;
+    y = y * 50 / duration;
+
+    SDL_Rect rect = {x, y, 50, 50};
+
+    if (*(ctx->snake->tail) == 1 || *(ctx->snake->tail) == -1)
+    {
+        rect.y += 2;
+        rect.h -= 4;
+    }
+    else
+    {
+        rect.x += 2;
+        rect.w -= 4;
+    }
+    SDL_SetRenderDrawColor(ctx->renderer, s.r, s.g, s.b, 255);
+    SDL_RenderFillRect(ctx->renderer, &rect);
+}
+
+void drawSnake(context *ctx)
+{
+    Scalar bodyColor = Scalar(255, 196, 225);
+    Scalar appleColor = Scalar(238, 215, 206);
+    Scalar HeadColor = Scalar(255, 135, 202);
+
+    int8_t *ptr = ctx->snake->tail;
+    // lastdir = *ptr;
+    while (ptr != ctx->snake->head)
+    {
+        drawBody(ctx, ptr, bodyColor);
+        ptr += *ptr;
+    }
+
+    drawApple(ctx, ctx->snake->applePos, appleColor);
+
+    drawHead(ctx, HeadColor, bodyColor);
+
+    drawTail(ctx, bodyColor);
+}
+void draw(context *ctx)
+{
+    SDL_SetRenderDrawColor(ctx->renderer, 234, 234, 234, 255);
+    SDL_RenderClear(ctx->renderer);
+
+    drawSnake(ctx);
+    SDL_RenderPresent(ctx->renderer);
+}
+
+void gameOver(context *ctx)
+{
+}
+
+void mainloop(void *arg)
+{
+    context *ctx = (context *)arg;
+    // std::cout << "Mat data: " << *(ctx->snake) << std::endl;
+    ctx->now = SDL_GetTicks();
+
+    if (ctx->now - ctx->start > ctx->duration)
+    {
+        ctx->start = ctx->now;
+        GS_DIRECTION in = input();
+        ctx->ris = ctx->snake->tick(in);
+    }
+    if (ctx->ris.size() == 0)
+    {
+        gameOver(ctx);
+        // draw(ctx);
+    }
+    else
+    {
+        draw(ctx);
+    }
+}
+
+int main()
+{
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    int cols = 15;
+    int rows = 15;
+    SDL_CreateWindowAndRenderer(cols * 50, rows * 50, 0, &window, &renderer);
+
+    Snake snake = Snake(cols, rows);
+    
+    std::vector<RenderIns> Rins = std::vector<RenderIns>();
+    int start = SDL_GetTicks();
+    int duration = 175;
+    context ctx = {renderer, &snake, Rins, start, start, duration};
+
+    const int simulate_infinite_loop = 1; // call the function repeatedly
+    const int fps = 0;                    // call the function as fast as the browser wants to render (typically 60fps)
+
+    std::cout << "Snake version 0.0.1-1" << std::endl;
+    emscripten_set_main_loop_arg(mainloop, &ctx, fps, simulate_infinite_loop);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
